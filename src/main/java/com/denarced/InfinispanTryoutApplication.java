11a4ae261589e4b69142e4b05860917b3b6523b1
denarced <denarced@gmail.com>
2017-05-07 21:02:32 +0300

    Init.

---
+++ src/main/java/com/denarced/InfinispanTryoutApplication.java
@@ -0,0 +1,11 @@
+package com.denarced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinispanTryoutApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InfinispanTryoutApplication.class, args);
+    }
+}
